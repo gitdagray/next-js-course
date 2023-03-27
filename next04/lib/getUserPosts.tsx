4329e69a976732150f99825b6411280f0524bd00
gitdagray <DaveOnEleven@gmail.com>
2023-03-26 19:05:09 -0500

    Added missing return keyword for undefined result

--- next04/lib/getUserPosts.tsx
+++ next04/lib/getUserPosts.tsx
@@ -1,7 +1,7 @@
 export default async function getUserPosts(userId: string) {
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`, { next: { revalidate: 60 } })
 
-    if (!res.ok) undefined
+    if (!res.ok) return undefined
 
     return res.json()
 }
